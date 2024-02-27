ead622b595c3b43a86c5ca8f112ed4b8ed30a389
leah-cook <156127890+leah-cook@users.noreply.github.com>
2024-02-27 11:45:07 -0700

    Lab 2

--- src/hdl/top_basys3.vhd
+++ src/hdl/top_basys3.vhd
@@ -79,23 +79,30 @@ end top_basys3;
 architecture top_basys3_arch of top_basys3 is 
 	
   -- declare the component of your top-level design unit under test (UUT)
+component sevenSegDecoder is 
+port ( 
+          i_D : in STD_LOGIC_VECTOR (3 downto 0);
+          o_S : out STD_LOGIC_VECTOR (6 downto 0)
+          );
+end component sevenSegDecoder;
 
-
+signal w_7SD_EN_n : std_logic;
+signal w_S : std_logic;
   -- create wire to connect button to 7SD enable (active-low)
-
   
 begin
 	-- PORT MAPS ----------------------------------------
 
 	--	Port map: wire your component up to the switches and seven-segment display cathodes
 	-----------------------------------------------------	
-	
-	
+ssd_inst : sevenSegDecoder	
+	port map ( 
+              i_D => sw,
+              o_S => seg
+              );
 	-- CONCURRENT STATEMENTS ----------------------------
-	
-	-- wire up active-low 7SD anode (active low) to button (active-high)
-	-- display 7SD 0 only when button pushed
-	-- other 7SD are kept off
+    w_7SD_EN_n  <= not btnC;
+    an  <= (0 => w_7SD_EN_n, others => '1');
 	-----------------------------------------------------
 	
 end top_basys3_arch;
